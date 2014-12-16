71c309f8cbe05a502ed1a0cd7b2a10e6087d0464
mchivallati <mchivallati@gmail.com>
2014-12-16 08:45:27 -0500

    Started class logic

--- src/net/franklinps/chivallati/GraphicsLab05/Box.java
+++ src/net/franklinps/chivallati/GraphicsLab05/Box.java
@@ -3,6 +3,28 @@ package net.franklinps.chivallati.GraphicsLab05;
 /**
  * Created by CompSci-04 on 12/16/2014.
  */
-public class Box
+public class Box extends Point
 {
+
+	private Point boxCenter;
+	private Point boxTopLeft;
+	private Point boxTopRight;
+	private Point boxBottomLeft;
+	private Point boxBottomRight;
+
+	public Box() //assigns the default points to make a box
+	{
+
+
+
+	}
+
+	public Box( Point boxCenter , int boxWidth , int boxHeight )
+	{
+
+		this.boxCenter = boxCenter;
+		//assign values to all atributes
+
+	}
+
 }
