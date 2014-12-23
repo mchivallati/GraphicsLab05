9554270d9dc6f23fdb6f295aa1e7d98f8f0ef4e1
mchivallati <mchivallati@gmail.com>
2014-12-23 14:05:41 -0500

    started on the tree for the background scene

---
+++ src/net/franklinps/chivallati/GraphicsLab05/Tree.java
@@ -0,0 +1,18 @@
+package net.franklinps.chivallati.GraphicsLab05;
+
+import java.awt.*;
+
+/**
+ * Created by CompSci-04 on 12/23/2014.
+ */
+public class Tree
+{
+
+	public Tree( Graphics g )
+	{
+
+
+
+	}
+
+}
---
+++ src/net/franklinps/chivallati/GraphicsLab05/TreeLeaves.java
@@ -0,0 +1,8 @@
+package net.franklinps.chivallati.GraphicsLab05;
+
+/**
+ * Created by CompSci-04 on 12/23/2014.
+ */
+public class TreeLeaves
+{
+}
